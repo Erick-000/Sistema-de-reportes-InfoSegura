f315228b21ce6680461ae18ca1420ccd1c23508e
Erick-000 <erickmanuel238@gmail.com>
2024-11-21 22:04:19 -0500

    Se cambian algunas imagenes en el dashboard

--- layout/parte1.php
+++ layout/parte1.php
@@ -63,7 +63,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="<?php echo $URL; ?>" class="brand-link">
-        <img src="<?php echo $URL; ?>/public/images/logo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?php echo $URL; ?>/public/images/_dfc39567-7c5a-4fa3-97a2-71f5aeabfad6.jpeg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">InfoSegura</span>
       </a>
 
@@ -72,7 +72,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="<?php echo $URL; ?>/public/templeates/AdminLTE-3.2.0/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            <img src="<?php echo $URL; ?>/public/images/user-icon.png" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
             <a href="#" class="d-block"><?php echo $nombres_sesion; ?></a>
